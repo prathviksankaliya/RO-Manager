1f6da8a3eb6344402d80ddac2b2216c04efa3182
Shadow <prathviksankaliya@gmail.com>
2024-01-03 13:29:29 +0530

    customer Transactions UI

---
+++ app/src/main/java/com/itcraftsolution/romanager/Activities/CustomerTransactionActivity.java
@@ -0,0 +1,16 @@
+package com.itcraftsolution.romanager.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.itcraftsolution.romanager.R;
+
+public class CustomerTransactionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_transaction);
+    }
+}
