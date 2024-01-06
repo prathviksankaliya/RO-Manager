16457c048c48fc30abf1025f9dac256cc3680cd7
Shadow <prathviksankaliya@gmail.com>
2024-01-06 13:02:59 +0530

    jag and bottle price counter done

--- app/src/main/java/com/itcraftsolution/romanager/Activities/AddCustomerTransactionActivity.java
+++ app/src/main/java/com/itcraftsolution/romanager/Activities/AddCustomerTransactionActivity.java
@@ -3,7 +3,10 @@ package com.itcraftsolution.romanager.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Toast;
 
 import com.itcraftsolution.romanager.R;
 import com.itcraftsolution.romanager.databinding.ActivityAddCustomerTransactionBinding;
@@ -16,6 +19,8 @@ public class AddCustomerTransactionActivity extends AppCompatActivity {
     private ActivityAddCustomerTransactionBinding binding;
     private String custName, moneyStatus;
     private int totalBalance;
+    private int bottlePrice = 20, jagPrice = 25;
+    private int totalPriceOfJag = 0, totalPriceOfBottle = 0, temp = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +43,59 @@ public class AddCustomerTransactionActivity extends AppCompatActivity {
             }
         });
 
+        binding.edJag.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                temp = totalPriceOfBottle;
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if(editable.length() > 0){
+                    int noOfJag = Integer.parseInt(editable.toString());
+                    totalPriceOfJag = noOfJag * jagPrice;
+                    if(temp != 0){
+                        binding.edAddTransaction.setText(String.valueOf("₹ "+ (temp + totalPriceOfJag)));
+                    }else{
+                        binding.edAddTransaction.setText(String.valueOf("₹ "+totalPriceOfJag));
+                    }
+                }else {
+                    binding.edAddTransaction.setText(String.valueOf("₹ "+temp));
+                }
+            }
+        });
+
+        binding.edBottle.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                temp = totalPriceOfJag;
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if(editable.length() > 0){
+                    int noOfBottle = Integer.parseInt(editable.toString());
+                    totalPriceOfBottle = noOfBottle * bottlePrice;
+                    if(temp != 0){
+                        binding.edAddTransaction.setText(String.valueOf("₹ "+ (temp + totalPriceOfBottle)));
+                    }else{
+                        binding.edAddTransaction.setText(String.valueOf("₹ "+totalPriceOfBottle));
+                    }
+                }else{
+                    binding.edAddTransaction.setText(String.valueOf("₹ "+temp));
+                }
+            }
+        });
     }
     private void loadData(){
         totalBalance = getIntent().getIntExtra("totalBalance", 0);
